f818e148d32071b032589e5266edb1e935e46615
o-ifeanyi <onuifeanyi95@gmail.com>
2022-04-19 23:13:02 +0100

    Fix integration test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -6,41 +6,37 @@ import 'robot.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('Start up and time machine flow', (WidgetTester tester) async {
+    final robot = Robot(tester);
 
-  group('App test', () {
-    testWidgets('Start up and time machine flow', (WidgetTester tester) async {
-      final robot = Robot(tester);
+    await robot.startApp(const TimeMachineScreen());
 
-      await robot.startApp(const TimeMachineScreen());
+    expect(find.text('Try out our time machine'), findsOneWidget);
+    expect(find.text('How much would you have today if...'), findsOneWidget);
+    expect(find.text('Bitcoin'), findsOneWidget);
+    expect(find.text('Every day'), findsOneWidget);
+    expect(find.text('1 year ago'), findsOneWidget);
 
-      expect(find.text('Try out our time machine'), findsOneWidget);
-      expect(find.text('How much would you have today if...'), findsOneWidget);
-      expect(find.text('Bitcoin'), findsOneWidget);
-      expect(find.text('Every day'), findsOneWidget);
-      expect(find.text('1 year ago'), findsOneWidget);
+    await robot.tap(find.text('Check now! 🚀'));
 
-      await robot.tap(find.text('Check now! 🚀'));
+    await robot.waitFor(find.text('You would have'));
 
-      await robot.waitFor(find.text('You would have'));
+    expect(find.text('Try out our time machine'), findsNothing);
+    expect(find.text('How much would you have today if...'), findsNothing);
 
+    expect(find.text('Total invested'), findsOneWidget);
+    expect(find.text('Current value'), findsOneWidget);
+    expect(find.text('Percent increase'), findsOneWidget);
+    await Future.delayed(const Duration(seconds: 1));
 
-      expect(find.text('Try out our time machine'), findsNothing);
-      expect(find.text('How much would you have today if...'), findsNothing);
+    expect(find.text('Try again'), findsOneWidget);
+    await robot.tap(find.text('Try again'));
 
-      expect(find.text('Total invested'), findsOneWidget);
-      expect(find.text('Current value'), findsOneWidget);
-      expect(find.text('Percent increase'), findsOneWidget);
-      await Future.delayed(const Duration(seconds: 1));
+    expect(find.text('You would have'), findsNothing);
 
-      expect(find.text('Try again'), findsOneWidget);
-      await robot.tap(find.text('Try again'));
-
-      expect(find.text('You would have'), findsNothing);
-
-      expect(find.text('Try out our time machine'), findsOneWidget);
-      expect(find.text('How much would you have today if...'), findsOneWidget);
-      await Future.delayed(const Duration(seconds: 1));
-      await robot.destroy();
-    });
+    expect(find.text('Try out our time machine'), findsOneWidget);
+    expect(find.text('How much would you have today if...'), findsOneWidget);
+    await Future.delayed(const Duration(seconds: 1));
+    await robot.destroy();
   });
 }
